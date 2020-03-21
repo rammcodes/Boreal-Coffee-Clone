e6dc3dddc08891df5b43e3e10212a7863e07ace9
Ram Maheshwari <ram991011@gmail.com>
2020-03-21 22:48:01 +0530

    finished implementation of jebrew section

--- src/Views/Homeview/Homeview.jsx
+++ src/Views/Homeview/Homeview.jsx
@@ -127,7 +127,34 @@ class Homeview extends React.Component {
           </div>
         </section>
         <section className="brew">
-          <div class="layer"></div>s
+          <div class="layer"></div>
+          <div className="content">
+            <h4 className="short-title">JEBREW</h4>
+            <h2 className="title">CAN'T MAKE IT BOREAL?</h2>
+            <hr className="org-line" />
+            <div className="text">
+              Can't make it to Boreal? Whether you are looking for a hearty mug
+              of black coffee in the morning or an after-lunch espresso, learn
+              how to brew a perfect cup of coffee for your needs and tastes :
+            </div>
+            <div className="img-and-buy">
+              <div className="img-cont">
+                <img
+                  src="http://www.borealcoffee.ch/design/themes/boreal/assets/dist/images/content/blocks/jebrew-link.png"
+                  alt="acc"
+                  className="img-acc"
+                />
+              </div>
+              <button className="buy">
+                <span className="txt">TEACH ME HOW TO BREW</span>
+                <img
+                  src={require('../../assets/icons/right-arrow.png')}
+                  alt="right-arrow"
+                  className="right"
+                />
+              </button>
+            </div>
+          </div>
         </section>
       </div>
     )

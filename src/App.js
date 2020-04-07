fced2440030e1dfa716e2fcf234711aa0a19b562
Ram Maheshwari <ram991011@gmail.com>
2020-04-07 12:22:24 +0530

    Changed route info styling inside shopview

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
     return (
       <div className="app">
         <Topbar />
-        <Homeview />
+        <Shopview />
         <Footer />
       </div>
     )

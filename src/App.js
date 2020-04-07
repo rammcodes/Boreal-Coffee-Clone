d042870a0d1de922e2a3acbc796e4b3928b16c16
Ram Maheshwari <ram991011@gmail.com>
2020-04-07 12:15:39 +0530

    Made Footer a separate component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Homeview from './Views/Homeview/Homeview'
 import './App.scss'
 import Topbar from './Components/Topbar/Topbar'
 import Shopview from './Views/Shopview/Shopview'
+import Footer from './Components/Footer/Footer'
 
 class App extends React.Component {
   render() {
     return (
       <div className="app">
         <Topbar />
-        <Shopview />
+        <Homeview />
+        <Footer />
       </div>
     )
   }
